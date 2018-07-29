e06e8dc13e5d9753864357ff393b2d65c1a00d84
Fenggang Wu <fwu@fb.com>
2018-07-29 00:43:09 -0700

    DataBlockHashIndex: udpate benchmarking script to test num_buckets

--- script/db_bench_test.sh
+++ script/db_bench_test.sh
@@ -3,6 +3,7 @@
 : ${ks:=16}
 : ${vs:=32}
 : ${block_index:=binary}
+: ${num_buckets:=500}
 
 script_dir=$(dirname $(readlink -f $0))
 rocksdb_dir=$(dirname ${script_dir})
@@ -22,6 +23,7 @@ $DB_BENCH  --data_block_index_type=${block_index} \
            --benchmarks=fillseq --compression_type=snappy \
            --statistics=true --block_restart_interval=1 \
            --compression_ratio=0.4 \
+           --block_hash_num_buckets=${num_buckets}
 
 time perf record -g -F 99 \
 $DB_BENCH  --data_block_index_type=${block_index} \
@@ -37,6 +39,7 @@ $DB_BENCH  --data_block_index_type=${block_index} \
            --compression_ratio=0.4 \
            --cache_size=10000000000 \
            --compressed_cache_size=10000000000 \
+           --block_hash_num_buckets=${num_buckets} \
            > readrand_${block_index}.log
 
 #           --read_cache_size=200000000
@@ -44,7 +47,7 @@ $DB_BENCH  --data_block_index_type=${block_index} \
 
 
 perf script | stackcollapse-perf.pl > out.perf-folded
-perf_figure=/mnt/public/fwu/k${ks}v${vs}_${block_index}_opt.svg
+perf_figure=/mnt/public/fwu/k${ks}v${vs}n${num_buckets}_${block_index}_opt.svg
 flamegraph.pl out.perf-folded > ${perf_figure}
 
 micros_op=$(grep readrandom readrand_${block_index}.log | awk '{print $3}')
@@ -53,4 +56,4 @@ bw=$(grep readrandom readrand_${block_index}.log | awk '{print $7}')
 cpu_util=$(grep -Po "<title>rocksdb::DataBlockIter::Seek.*samples, \K[1-9]+\.[^%]+" ${perf_figure})
 space=$(du /dev/shm/${block_index} | grep -Po '^\d+')
 
-echo $ks, ${vs}, ${block_index}, ${micros_op}, ${ops_sec}, ${bw}, ${cpu_util}, ${space}| tee -a k.log
+echo $ks, ${vs}, ${block_index}, ${num_buckets} ${micros_op}, ${ops_sec}, ${bw}, ${cpu_util}, ${space}| tee -a k.log
--- script/multi_db_bench_test.sh
+++ script/multi_db_bench_test.sh
@@ -3,11 +3,19 @@ script_dir=$(dirname $(readlink -f $0))
 rocksdb_dir=$(dirname ${script_dir})
 
 ks=8
+vs=40
 #: > k${ks}-binary.log
 #: > k${ks}-hash.log
 #for vs in 20 60 100 140; do
-for vs in 40; do
-    echo $vs
-    (vs=${vs} ks=${ks} block_index=binary ${script_dir}/db_bench_test.sh)
-    (vs=${vs} ks=${ks} block_index=hash ${script_dir}/db_bench_test.sh)
+#for vs in 40; do
+#    echo $vs
+#    (vs=${vs} ks=${ks} block_index=binary ${script_dir}/db_bench_test.sh)
+#    (vs=${vs} ks=${ks} block_index=hash ${script_dir}/db_bench_test.sh)
+#done
+
+(vs=${vs} ks=${ks} block_index=binary ${script_dir}/db_bench_test.sh)
+for buck in 1 4 16 64 256 512; do
+    echo vs=${vs} ks=${ks} block_index=hash num_buckets=${buck}
+    continue
+    (vs=${vs} ks=${ks} block_index=hash num_buckets=${buck} ${script_dir}/db_bench_test.sh)
 done
