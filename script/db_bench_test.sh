44bb01efbb11a54d0e6421bd7ac636ca0d5262bf
Fenggang Wu <fwu@fb.com>
2018-08-09 18:12:52 -0700

    DataBlockHashIndex: Update test script
    Summary:
    add interval test
    add thread test
    add ks vs test
    add cache test
    Add time stamp

--- script/db_bench_test.sh
+++ script/db_bench_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-: ${ks:=16}
-: ${vs:=32}
+: ${ks:=8}
+: ${vs:=40}
 : ${block_index:=binary}
 : ${num_buckets:=500}
 : ${num:=200000000}
-: ${reads:=10000000}
+: ${reads:=100000000}
 : ${threads:=1}
 : ${restart_interval:=16}
-: ${util_ratio:=0.75}
-
+: ${util_ratio:=0.8}
+: ${cache_size:=20000000000} #20GB
 #test_prefix=k${ks}v${vs}t${threads}_${block_index}${num_buckets}
 
 script_dir=$(dirname $(readlink -f $0))
@@ -19,10 +19,10 @@ DB_BENCH=${rocksdb_dir}/db_bench
 
 db_dir=/home/fwu/db
 
-
+ts=$(date +"%Y-%m-%d_%H-%M-%S")
 dataset_prefix=k${ks}_v${vs}_N$(($num/1000000))
 index_prefix=RI${restart_interval}_${block_index}${util_ratio}
-read_prefix=R$(($reads/1000000))_t${threads}
+read_prefix=R$(($reads/1000000))_t${threads}_cache$(($cache_size/1000000000))
 
 
 db=${db_dir}/${dataset_prefix}/${index_prefix}
@@ -46,16 +46,17 @@ fi
 test_prefix=${read_prefix}_${index_prefix}
 write_log=${log_dir}/fillseq_${test_prefix}.log
 read_log=${log_dir}/readrand_${test_prefix}.log
-fig_name=${test_prefix}_$(date +"%Y-%m-%d_%H-%M-%S").svg
+perf_log=${log_dir}/perf_${test_prefix}.log
+fig_name=${test_prefix}_${ts}.svg
 perf_figure=${fig_dir}/${fig_name}
 echo ---------------------------------------------------------------------
 echo test:" "${dataset_prefix}_${test_prefix}
-echo R_log:""$(readlink -f ${read_log})
 echo W_log:""$(readlink -f ${write_log})
+echo R_log:""$(readlink -f ${read_log})
 #echo fig_dir:""${perf_figure}
-echo fig:"  "https://home.fburl.com/~fwu/${fig_name}
+echo fig:"  "https://home.fburl.com/~fwu/${dataset_prefix}/${fig_name}
 echo ---------------------------------------------------------------------
-
+:<<END
 $DB_BENCH  --data_block_index_type=${block_index} \
            --db=${db} \
            --block_size=16000 --level_compaction_dynamic_level_bytes=1 \
@@ -70,8 +71,7 @@ $DB_BENCH  --data_block_index_type=${block_index} \
            --statistics=true \
            --perf_level=2 \
            >${write_log}
-
-
+END
 time perf record -g -F 99 \
 $DB_BENCH  --data_block_index_type=${block_index} \
            --db=${db} \
@@ -85,7 +85,7 @@ $DB_BENCH  --data_block_index_type=${block_index} \
            --compression_type=snappy \
            --block_restart_interval=16 \
            --compression_ratio=0.4 \
-           --cache_size=20000000000 \
+           --cache_size=${cache_size} \
            --block_hash_num_buckets=${num_buckets} \
            --data_block_hash_table_util_ratio=${util_ratio} \
            --use_direct_reads \
@@ -98,7 +98,7 @@ $DB_BENCH  --data_block_index_type=${block_index} \
 #           --read_cache_size=200000000
 
 
-
+#perf report > ${perf_log}
 perf script | stackcollapse-perf.pl > out.perf-folded
 flamegraph.pl out.perf-folded > ${perf_figure}
 
@@ -113,9 +113,16 @@ hash_fallback=$(grep hash.index.fallback ${read_log} | awk '{print $4}')
 hash_success=$(grep hash.index.success ${read_log} | awk '{print $4}')
 hash_total=$((${hash_fallback} + ${hash_success}))
 fallback_ratio=$(python -c "print(1.0*${hash_fallback}/${hash_total})")
-echo ${dataset_prefix}_${read_prefix}_${index_prefix}, \
+
+block_cache_miss=$(grep rocksdb.block.cache.miss ${read_log} | awk '{print $4}')
+block_cache_hit=$(grep rocksdb.block.cache.hit ${read_log} | awk '{print $4}')
+block_cache_total=$((${block_cache_miss} + ${block_cache_hit}))
+cache_miss_ratio=$(python -c "print(1.0*${block_cache_miss}/${block_cache_total})")
+
+echo ${ts}, ${dataset_prefix}_${read_prefix}_${index_prefix}, \
      ${bw}, ${cpu_util}, ${space}, ${hash_fallback}, ${hash_total}, \
-     ${fallback_ratio} | tee -a k.log
+     ${fallback_ratio}, ${block_cache_miss}, ${block_cache_hit}, ${block_cache_total}, \
+     ${cache_miss_ratio}| tee -a k.log
 
 :<<END
 echo ${dataset_prefix}_${read_prefix}_${index_prefix}, ${micros_op}, ${ops_sec}, \
--- script/multi_db_bench_test.sh
+++ script/multi_db_bench_test.sh
@@ -39,13 +39,73 @@ for threads in 10 20 30 40 50; do
 done
 END
 
-for restart_interval in 1; do
+:<<END
+#RI and util ratio
+for restart_interval in 1 4 16; do
     (vs=${vs} ks=${ks} restart_interval=${restart_interval} \
        block_index=binary \
        ${script_dir}/db_bench_test.sh)
-    for util_ratio in 1 0.8 0.6 0.4 0.2; do
+    for util_ratio in 1 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1; do
         (vs=${vs} ks=${ks} restart_interval=${restart_interval} \
            block_index=hash num_buckets=${buck} util_ratio=${util_ratio} \
+           ${script_dir}/db_bench_test.sh)
+    done
+done
+END
+
+:<<END
+# thread number
+restart_interval=16
+for threads in 1 4 16; do
+    (vs=${vs} ks=${ks} restart_interval=${restart_interval} \
+       block_index=binary threads=${threads}\
+       ${script_dir}/db_bench_test.sh)
+    for util_ratio in 0.8; do
+        (vs=${vs} ks=${ks} restart_interval=${restart_interval} \
+           block_index=hash num_buckets=${buck} util_ratio=${util_ratio} \
+           threads=${threads} \
+           ${script_dir}/db_bench_test.sh)
+    done
+done
+END
+
+:<<END
+# ks
+for ks in 10 20 30 40 50; do
+    (ks=${ks} block_index=binary \
+       ${script_dir}/db_bench_test.sh)
+    for util_ratio in 0.8; do
+        (ks=${ks} block_index=hash num_buckets=${buck} util_ratio=${util_ratio} \
+           ${script_dir}/db_bench_test.sh)
+    done
+done
+
+# vs
+for vs in 20 40 60 80 100; do
+    (vs=${vs} block_index=binary \
+       ${script_dir}/db_bench_test.sh)
+    for util_ratio in 0.8; do
+        (vs=${vs} block_index=hash num_buckets=${buck} util_ratio=${util_ratio} \
+           ${script_dir}/db_bench_test.sh)
+    done
+done
+END
+
+
+# cache
+restart_interval=1
+threads=16
+for cache_size in 1000000000; do
+    (vs=${vs} ks=${ks} restart_interval=${restart_interval} \
+       block_index=binary \
+       threads=${threads} \
+       cache_size=${cache_size} \
+       ${script_dir}/db_bench_test.sh)
+    for util_ratio in 1 0.9 0.8 0.7 0.6 0.5; do
+        (vs=${vs} ks=${ks} restart_interval=${restart_interval} \
+           block_index=hash num_buckets=${buck} util_ratio=${util_ratio} \
+           threads=${threads} \
+           cache_size=${cache_size} \
            ${script_dir}/db_bench_test.sh)
     done
 done
